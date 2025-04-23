1c25a70571de16f4a74f9bf82afea08e28d3a7f2
Jenifer <mexajeni@gmail.com>
2025-04-23 13:44:27 +0530

    adding changed script

--- colorflipper/script.js
+++ colorflipper/script.js
@@ -1,16 +1,16 @@
 const btn = document.getElementById("flip");
 
-const colorArray = [
-  "red",
-  "blue",
-  "green",
-  "yellow",
-  "pink",
-  "orange",
-  "purple",
-];
+function generateRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color = color + letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
 
 btn.addEventListener("click", () => {
-  let randomNumber = Math.floor(Math.random() * 7);
-  document.body.style.backgroundColor = colorArray[randomNumber];
+  let color = generateRandomColor();
+  document.body.style.backgroundColor = color;
 });
